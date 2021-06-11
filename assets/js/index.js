8ddfa66994350d9c86dcdb94bc841644779062c2
Guilherme José <guilherme.2010.guilherme@gmail.com>
2021-06-10 22:56:28 -0300

    Finalização das páginas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 seta = document.getElementById("seta");
 
+
+
 function MoverTela(){
     window.scroll(0, vh(100) - 100);
 }
@@ -9,3 +11,4 @@ function vh(v) {
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return (v * h) / 100;
 }
+
